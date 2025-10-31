dd47c9febe7e28435f912f37fefb9a699b15f879
vanessaZunigaL <b.zuniga@uniandes.edu.co>
2025-10-30 23:06:58 -0500

    Feat: enhance vehicle list display and add brand totals

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -10,17 +10,30 @@ import { Vehicle } from '../vehicle';
 export class VehicleListComponent implements OnInit {
 
   vehicles: Vehicle[] = [];
+  counterVehicles: { [key: string]: number } = {};
 
   constructor( private vehicleService: VehicleService) { }
+
+  updateTotals() {
+    this.counterVehicles = this.vehicles.reduce((acc, vehicle) => {
+      acc[vehicle.marca] = (acc[vehicle.marca] || 0) + 1;
+      return acc;
+    }, {} as Record<string, number>);
+    console.log(this.counterVehicles);
+  }
 
   getVehicles(): void {
     this.vehicleService.getVehicles().subscribe((vehicles) => {
       this.vehicles = vehicles;
+      this.updateTotals()
     });
   }
 
+  getBrands(): string[] {
+    return Object.keys(this.counterVehicles);
+  }
+  
   ngOnInit() {
     this.getVehicles();
   }
-
 }
