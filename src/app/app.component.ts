66059fd6d03f16cc5b59944760df6f539302d3b6
vanessaZunigaL <b.zuniga@uniandes.edu.co>
2025-10-30 21:23:55 -0500

    Initialize Angular project with basic structure and configuration

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Parcial-1-MISW-4104';
+}
